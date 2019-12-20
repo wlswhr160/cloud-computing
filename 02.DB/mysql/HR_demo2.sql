3327526c9154af66b54ed21c76dbc0bbc6455b29
Dowon Lee <edowon0623@gmail.com>
2019-12-20 13:53:15 +0900

    upload new sql sample

--- 02.DB/mysql/HR_demo2.sql
+++ 02.DB/mysql/HR_demo2.sql
@@ -1,14 +1,26 @@
 [6] 각 이름이 ‘s’로 끝나는 사원들의 이름과 업무를 아래의 예와 같이 출력하고자 한다. 출력 시 성과 이름은 첫 글자가 대문자, 업무는 모두 대문자(UPPER함수 사용)로 출력하고 머리글은 Employee JOBs로 표시하시오.
 	□예 Sigal Tobias is a PU_CLERK
-
+select concat(first_name, ' ', last_name, ' is a ', UPPER(job_id)) 
+			as "Employee JOBs"
+from employees        
+-- where last_name like '%s';
+where substr(last_name, -1, 1)='s';
 
 [7] 모든 사원의 연봉을 표시하는 보고서를 작성하려고 한다. 보고서에 사원의 성과 이름(Name으로 별칭), 급여, 수당여부에 따른 연봉을 포함하여 출력하시오. 수당여부는 수당이 있으면 “Salary + Commission”, 수당이 없으면 “Salary only”라고 표시하고, 별칭은 적절히 붙인다. 또한 출력 시 연봉이 높은 순으로 정렬한다. 
 	- IF, IFNULL
-
+SELECT concat(first_name, ' ', last_name) as 'Name', salary
+    , (salary * 12) 
+		+ (salary * 12 * ifnull(commission_pct, 0)) as 'Annual Salary'
+	, if(commission_pct is null, 'Salary only', 'Salary + Commission') as 'Salary Type'
+FROM Employees
+order by 3 desc;
 
 [8] 모든 사원들 성과 이름(Name으로 별칭), 입사일 그리고 입사일이 어떤 요일이였는지 출력하시오. 이때 주(week)의 시작인 일요일부터 출력되도록 정렬하시오.
 	- DATE_FORMAT()
-
+SELECT concat(first_name, ' ', last_name) as 'Name', hire_date
+	, date_format(hire_date, '%W') as 'Day of the week'
+FROM employees
+order by date_format(hire_date, '%w')
 	
 [9] 모든 사원은 직속 상사 및 직속 직원을 갖는다. 단, 최상위 또는 최하위 직원은 직속상사 및 직원이 없다. 소속된 사원들 중 어떤 사원의 상사로 근무 중인 사원의 총 수를 출력하시오.
 
